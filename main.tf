6ea782a7576fae1e8695517770877bc6d0e3b547
Venugopal <venkadali@gmail.com>
2025-05-20 19:32:24 +0530

    Eighth commit

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-venugopal"
+    key            = "env/dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
@@ -8,5 +18,14 @@ resource "aws_instance" "web" {
 
   tags = {
     Name = "jenkins-demo-instance"
+  }
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "my-unique-bucket-from-jenkins-demo-001"
+
+  tags = {
+    Name        = "DemoBucket"
+    Environment = "Dev"
   }
 }
